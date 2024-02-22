48db492cec511f5175c0bde8d313004b22c80fbd
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-22 11:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,28 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+  var number = document.getElementById("numberInput").value;
+      if (isNaN(number) || number < 1 || number > 3999) {
+        document.getElementById("result").innerText = "Please enter a valid number between 1 and 3999.";
+        return;
+      }
+
+      var romanNumerals = [
+        ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+        ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+        ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+        ["", "M", "MM", "MMM"]
+      ];
+
+      var romanNumeral = "";
+      var numString = number.toString();
+
+      for (var i = numString.length - 1, j = 0; i >= 0; i--, j++) {
+        romanNumeral = romanNumerals[j][parseInt(numString[i])] + romanNumeral;
+      }
+
+      document.getElementById("result").innerText = "Roman numeral: " + romanNumeral;
+    }
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
