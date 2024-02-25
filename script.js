42a916977336382b7cd65f2760a7533edd34118d
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-25 18:07:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-  function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  function convertToRoman(num) {	
 
   var number = document.getElementById("numberInput").value;
       if (isNaN(number) || number < 1 || number > 3999) {
